ca091eb5dbb137ba4c62568682788a1ca0a04bb8
Sean Murphy <sean@iamseanmurphy.com>
2026-05-08 16:44:10 +0000

    1.6.2

--- alante/functions.php
+++ alante/functions.php
@@ -6,7 +6,7 @@
  */
 
 // Declare latest theme version
-$GLOBALS['alante_thinkup_theme_version'] = '1.6.1';
+$GLOBALS['alante_thinkup_theme_version'] = '1.6.2';
 
 // Setup content width
 function alante_thinkup_content_width() {
